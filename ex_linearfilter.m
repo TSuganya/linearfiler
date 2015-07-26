1bda0fd1258ab9d6912dd40e0ac635a68d09f13d
Suganya Thurairatnam <kichaa4@gmail.com>
2015-07-26 14:18:50 +0530

    3rd commit

--- ex_linearfilter.m
+++ ex_linearfilter.m
@@ -1,4 +1,4 @@
-I=imread('..\images\noisy.png');
+I=imread('..\images\cameraman.jpg');
 if (length(size(I))==3)
     I=rgb2gray(I);
 end
@@ -7,6 +7,10 @@ end
 subplot(2,4,1);imshow(I);title('original')
 subplot(2,4,5);h=histogram(I);plot(h);
 
+F=ones(5,5);
+J=avgfilter(I,F);
+subplot(2,4,2);imshow(J);title('average filtered')
+subplot(2,4,6);h=histogram(J);plot(h);
 
 G=[0 1 2 1 0;1 3 5 3 1;2 5 9 5 2;1 3 5 3 1;0 1 2 1 0];
 K=gaussian(I,G);
