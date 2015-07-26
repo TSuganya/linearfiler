5c2fcfa2d52698c9b6ae064dde4a23feb429e796
Suganya Thurairatnam <kichaa4@gmail.com>
2015-07-26 13:50:12 +0530

    first commit

---
+++ gaussian.m
@@ -0,0 +1,28 @@
+function J = gaussian(I,G)
+if (length(size(I))==3)
+    I=rgb2gray(I);
+end
+
+[r,c]=size(I);
+J=ones(r,c);
+
+K=floor(size(G,1)/2);
+L=floor(size(G,2)/2);
+
+for x=K+1:r-K-1
+    for y=L+1:c-L-1
+        m=[];
+        for i=-K:K
+            for j=-L:L
+                a=I(x+i,y+j)*G(i+K+1,j+L+1);
+                m=[m a];
+            end
+        end
+        
+        J(x,y)=mean(m);
+    end
+end
+J=uint8(J);
+
+
+    
